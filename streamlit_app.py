1d6d39f9e11554dce0eb8776c0712d2da91928b6
CodeHawke <129512266+CodeHawke@users.noreply.github.com>
2023-04-01 12:46:59 +0530

    Update streamlit_app.py
    added menu and menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Idli')
+streamlit.text('Oats')
+streamlit.text('Cornflakes')
